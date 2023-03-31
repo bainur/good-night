833f8a69738efa328512d00e601f0c1a91f95faf
bainur <mbainurb@gmail.com>
2023-03-31 12:39:11 +0700

    README

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -2,11 +2,13 @@ class Api::V1::UserSerializer
   include JSONAPI::Serializer
 
   attributes :name, :id
-  has_many :sleep_records
-
-  has_many :follows, serializer: Api::V1::FollowSerializer
-
   attribute :followers do |object|
     object.followed_users
   end
+
+  attribute :following do |object|
+    object.follows
+  end
+
+  has_many :sleep_records
 end
